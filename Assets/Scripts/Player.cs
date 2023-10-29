cb18a92fea71d050d48d8968f5c040a4e4f3461c
alex2209alex <alex.pavel2002@gmail.com>
2023-10-29 19:38:11 +0200

    Squashed commit of the following:
    commit 9c42d6ea95d6dfd20437b16b2193843e336223b4
    Author: alex2209alex <alex.pavel2002@gmail.com>
    Date:   Sun Oct 29 19:37:24 2023 +0200
    
        Player now looks towards mouse
    
        Player now looks towards mouse
    
    commit 51b77073ccc4669319e09cee4953f0a32ab91d2f
    Author: alex2209alex <alex.pavel2002@gmail.com>
    Date:   Sat Oct 21 04:08:57 2023 +0300
    
        Player movement and camera
    
        Player movement and camera. Top down view.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementSpeed;
+
+    void Awake() {
+        rb = gameObject.GetComponent<Rigidbody>();
+        movementSpeed = 5f;
+    }
+
+    void FixedUpdate() 
+    {
+        LookAtMouse();
+        MovePlayer();
+    }
+
+    private void LookAtMouse()
+    {
+        Vector3 mouseDirection = GetMouseDirection();
+        transform.rotation = Quaternion.LookRotation(mouseDirection - transform.position);
+    }
+
+    private Vector3 GetMouseDirection() {
+        Vector3 mouseDirection = Input.mousePosition;
+        mouseDirection.z = 7.5f;
+        return Camera.main.ScreenToWorldPoint(mouseDirection);
+    }
+    
+    private void MovePlayer()
+    {
+        rb.MovePosition(rb.position + GetInputForMovement() * movementSpeed * Time.fixedDeltaTime);
+    }
+
+    private Vector3 GetInputForMovement()
+    {
+        return new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+}
